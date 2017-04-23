acda8ce4b013a5553882a95c172415f66f2aad3f
wangpf <wangpf2011@163.com>
2017-04-23 23:30:50 +0800

    TestCase

---
+++ frame/src/androidTest/java/com/lnint/frame/ExampleInstrumentedTest.java
@@ -0,0 +1,32 @@
+package com.lnint.frame;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.lnint.frame.test", appContext.getPackageName());
+    }
+
+    @Test
+    public void testAddActivity() {
+        assertEquals(0, 1);//方法第一个参数的意思是，我们预期的值；第二个参数的意思是，实际结果值。很显然它会报错。
+        assertEquals(1, 1);//这个就不会报错啦。
+    }
+}
